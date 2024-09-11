67099048f5bfb485285f0c4cdbf32ac7d38d3e2f
Insaram Alam Siddique <alaminsaram92@gmail.com>
2024-09-11 16:08:13 +0500

    Initial Commit

---
+++ LibraryApp/LibraryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAppApp.swift
+//  LibraryApp
+//
+//  Created by Insaram Alam Siddique on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LibraryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
