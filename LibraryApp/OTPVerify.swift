78c634cb5bba2a6e5df8f6f793621646561b52fd
Insaram Alam Siddique <alaminsaram92@gmail.com>
2024-10-24 01:36:09 +0500

    Verify Modal Added

--- LibraryApp/OTPVerify.swift
+++ LibraryApp/OTPVerify.swift
@@ -4,21 +4,58 @@ struct OTPVerify: View {
     @State private var email: String = ""
     @State private var password: String = ""
     @State private var isPasswordVisible: Bool = false
+    @State private var isModalVisible: Bool = false
 
     var body: some View {
         BaseLayout(cgb:true){
-            VStack {
-                
-                TopHeader(heading: "Email Verification", description: "Your account verification code has been Sent to your email address")
-                
-                VStack(spacing:35){
-                    OTPInputView()
-                    CustomButton(title: "Verify", onPress: {})
-             }
-                Spacer()
+            ZStack{
+                VStack {
+                    
+                    TopHeader(heading: "Email Verification", description: "Your account verification code has been Sent to your email address")
+                    
+                    VStack(spacing:35){
+                        OTPInputView()
+                        CustomButton(title: "Verify", onPress: {
+                            withAnimation {
+                                isModalVisible.toggle()
+                            }
+                        })
+                    }
+                    Spacer()
+                    
+                }
+                .padding(30)
+            
+                if isModalVisible {
+                    ModalView(isVisible: $isModalVisible) {
+                        VStack {
+                            Image("emailSent")
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                                .foregroundColor(.primaryPurple)
+
+                            Text("Email Verified Successfully!")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.primaryPurple)
+
+                            Button("Great") {
+                                withAnimation {
+                                    isModalVisible.toggle()
+                                }
+                            }
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.primaryPurple)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                        }
+                    }
+                }
                 
             }
-            .padding(30)
         }
     }
 }
